317d2a45c9c3905d511e796436f28286aaded37d
Yuwol6 <juneyoung.jeong66@outlook.com>
2025-12-29 15:57:34 +1000

    Add base logic
    Added logic to calculate the minimum bid price to break even.

--- CoinProfitTracker/Program.cs
+++ CoinProfitTracker/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System.Net.Http;
 using System.Text.Json;
+using System.Globalization;
 
-Console.WriteLine("Hello, World!");
 
+// pulls API and sets initial constants
 const string bitCoin = "BTC";
 const decimal tradingFee = 0.001m;
 string url = $"https://www.coinspot.com.au/pubapi/v2/latest/{bitCoin}";
@@ -10,14 +11,31 @@ string url = $"https://www.coinspot.com.au/pubapi/v2/latest/{bitCoin}";
 using var http = new HttpClient();
 string jsonText = await http.GetStringAsync(url);
 
-Console.WriteLine(jsonText);
+Console.WriteLine("Calling CoinSpot API...");
+Console.WriteLine();
 
 using JsonDocument doc = JsonDocument.Parse(jsonText);
 JsonElement root = doc.RootElement;
-string status = root.GetProperty("status").ToString();
 
-Console.WriteLine($"Status from JSON: {status}");
+// check the status
+string status = root.GetProperty("status").GetString()!;
+if (status != "ok")
+{
+    Console.WriteLine("API request failed.");
+    return;
+}
 
-JsonElement prices = root.GetProperty("prices");
-string ask = prices.GetProperty("ask").ToString();
-Console.WriteLine(ask);
+JsonElement prices = root.GetProperty("prices"); // get ask and bid as decimal variable
+
+decimal ask = decimal.Parse(prices.GetProperty("ask").GetString()!, CultureInfo.InvariantCulture);
+Console.WriteLine($"The ask (buying price) is: ${ask:F2}");
+decimal bid = decimal.Parse(prices.GetProperty("bid").GetString()!, CultureInfo.InvariantCulture);
+Console.WriteLine($"The bid (selling price) is: ${bid:F2}");
+
+// produce formula to break even, including fees
+decimal breakEvenBid = ask * (1 + tradingFee) / (1 - tradingFee);
+
+// calculate break-even price
+decimal differencePercentage = (breakEvenBid - bid)/bid * 100;
+Console.WriteLine();
+Console.WriteLine($"To break even, the bid must rise {differencePercentage:F2}% to ${breakEvenBid:F2}");
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 A small C# project that fetches CoinSpot cryptocurrency prices and calculates trading profit transition points, using a REST API and JSON parsing.
+
