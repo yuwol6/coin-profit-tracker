8890191a6454a27a25ebe68b32933b2c1f590c35
Yuwol6 <juneyoung.jeong66@outlook.com>
2025-12-30 14:34:16 +1000

    Add other profit percentages
    Added 5% and 10% profit calculations, and modularised the logic into helper methods for improved extensibility.

--- CoinProfitTracker/Program.cs
+++ CoinProfitTracker/Program.cs
@@ -31,11 +31,36 @@ decimal ask = decimal.Parse(prices.GetProperty("ask").GetString()!, CultureInfo.
 Console.WriteLine($"The ask (buying price) is: ${ask:F2}");
 decimal bid = decimal.Parse(prices.GetProperty("bid").GetString()!, CultureInfo.InvariantCulture);
 Console.WriteLine($"The bid (selling price) is: ${bid:F2}");
+if (bid > ask) Console.WriteLine("Warning: the bid is higher than ask; results and calculations may be off.");
 
 // produce formula to break even, including fees
 decimal breakEvenBid = ask * (1 + tradingFee) / (1 - tradingFee);
 
 // calculate break-even price
-decimal differencePercentage = (breakEvenBid - bid)/bid * 100;
+decimal percentageToBreakEven = (breakEvenBid - bid)/bid * 100;
 Console.WriteLine();
-Console.WriteLine($"To break even, the bid must rise {differencePercentage:F2}% to ${breakEvenBid:F2}");
+Console.WriteLine($"To break even, the bid must rise by {percentageToBreakEven:F2}% to ${breakEvenBid:F2}");
+
+// calculate different profit points
+decimal onePercentProfitBid = GetBidForPercentProfit(0.01m, breakEvenBid);
+decimal percentageToOnePercentProfit = GetPercentageToBid(onePercentProfitBid, bid);
+Console.WriteLine($"For a 1% profit, the bid must rise by {percentageToOnePercentProfit:F2}% to ${onePercentProfitBid:F2}");
+
+decimal fivePercentProfitBid = GetBidForPercentProfit(0.05m, breakEvenBid);
+decimal percentageToFivePercentProfit = GetPercentageToBid(fivePercentProfitBid, bid);
+Console.WriteLine($"For a 5% profit, the bid must rise by {percentageToFivePercentProfit:F2}% to ${fivePercentProfitBid:F2}");
+
+decimal tenPercentProfitBid = GetBidForPercentProfit(0.1m, breakEvenBid);
+decimal percentageToTenPercentProfit = GetPercentageToBid(tenPercentProfitBid, bid);
+Console.WriteLine($"For a 10% profit, the bid must rise by {percentageToTenPercentProfit:F2}% to ${tenPercentProfitBid:F2}");
+
+
+decimal GetBidForPercentProfit(decimal targetProfitDecimal, decimal baseBid)
+{
+    return baseBid * (1 + targetProfitDecimal);
+}
+
+decimal GetPercentageToBid(decimal targetBid, decimal baseBid)
+{
+    return (targetBid - baseBid)/baseBid * 100;
+}
