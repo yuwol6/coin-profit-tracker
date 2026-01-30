f20c3341a5195bac3c3fb8bb70995b8794642289
Yuwol <juneyoung.jeong66@outlook.com>
2026-01-30 12:23:40 +1000

    Add functionality to update profit targets on refresh.
    Added functionality to app.js to update profit targets' percentages and target bids on clicking the refresh button + on web app start.

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,33 +1,49 @@
 const refreshBtn = document.getElementById('refreshBtn');
 const askText = document.getElementById('ask-text');
 const bidText = document.getElementById('bid-text');
+const oneRisePercent = document.getElementById('one-rise-percent');
+const fiveRisePercent = document.getElementById('five-rise-percent');
+const tenRisePercent = document.getElementById('ten-rise-percent');
+const oneProfitBid = document.getElementById('one-profit-bid');
+const fiveProfitBid = document.getElementById('five-profit-bid');
+const tenProfitBid = document.getElementById('ten-profit-bid');
 getTradeMetrics();
 
 async function getTradeMetrics() {
   const metricResponse = await fetch('https://localhost:7297/api/prices');
   const metricData = await metricResponse.json();
 
-  console.log(metricData);
-
   displayMetrics(metricData);
 }
 
 function displayMetrics(metrics) {
-  const askMetrics = metrics.ask.toLocaleString('en-AU', {
-    style: 'currency',
-    currency: 'AUD',
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  });
-  const bidMetrics = metrics.bid.toLocaleString('en-AU', {
+  const askMetrics = changeNumToAud(metrics.ask);
+  const bidMetrics = changeNumToAud(metrics.bid);
+  const oneProfitMetrics = changeNumToAud(metrics.onePercentProfitBid);
+  const fiveProfitMetrics = changeNumToAud(metrics.fivePercentProfitBid);
+  const tenProfitMetrics = changeNumToAud(metrics.tenPercentProfitBid);
+
+  askText.textContent = askMetrics;
+  bidText.textContent = bidMetrics;
+
+  oneRisePercent.textContent = `${metrics.percentageToOnePercentProfit.toFixed(2)}%`;
+  fiveRisePercent.textContent = `${metrics.percentageToFivePercentProfit.toFixed(2)}%`;
+  tenRisePercent.textContent = `${metrics.percentageToTenPercentProfit.toFixed(2)}%`;
+
+  oneProfitBid.textContent = oneProfitMetrics;
+  fiveProfitBid.textContent = fiveProfitMetrics;
+  tenProfitBid.textContent = tenProfitMetrics;
+}
+
+function changeNumToAud(number) {
+  const returnNum = number.toLocaleString('en-AU', {
     style: 'currency',
     currency: 'AUD',
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   });
 
-  askText.textContent = askMetrics;
-  bidText.textContent = bidMetrics;
+  return returnNum;
 }
 
 refreshBtn.addEventListener('click', () => {
